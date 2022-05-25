690be98e831a5f283b96631bc6f14bee03b57f5a
hrooney18 <57280198+hrooney18@users.noreply.github.com>
2022-05-25 14:05:45 -0400

    Add files via upload

---
+++ Door-Scripts/FullGameDoorScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FullGameDoorScript : MonoBehaviour
+{
+    private bool touchingDoor = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (touchingDoor)
+            if (Input.GetKeyDown(KeyCode.Return))
+                SceneManager.LoadSceneAsync("Story Level 1");
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            touchingDoor = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            touchingDoor = false;
+        }
+    }
+}
